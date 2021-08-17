bc2ea5bd7d5baafa77bff6419892bf45c6950949
GinaShev <ginashev@gmail.com>
2021-08-17 16:35:07 +0200

    contact test
    create contact test + class Contact

---
+++ contact.py
@@ -0,0 +1,18 @@
+class Contact:
+
+    def __init__(self, fName, lName, title, company, address_1, hPhone, mPhone, email, bDay, bMonth, bYear,
+                 address_2, notes, homepage):
+        self.fName = fName
+        self.lName = lName
+        self.title = title
+        self.company = company
+        self.address_1 = address_1
+        self.hPhone = hPhone
+        self.mPhone = mPhone
+        self.email = email
+        self.bDay = bDay
+        self.bMonth = bMonth
+        self.bYear = bYear
+        self.address_2 = address_2
+        self.notes = notes
+        self.homepage = homepage
