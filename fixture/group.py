3ef2d320429ed49243cc9e2af187b386f79196dc
GinaShev <ginashev@gmail.com>
2021-08-18 07:33:24 +0200

    make common fixture

--- fixture/group.py
+++ fixture/group.py
@@ -25,3 +25,10 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        wd.find_element_by_name("selected[]").click()
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
