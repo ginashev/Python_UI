8636e7ad57cd2026b260aef5d0279e26b35a4b17
GinaShev <ginashev@gmail.com>
2021-09-05 22:11:19 +0200

    addition assert to groups

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,17 +4,26 @@ from model.group import Group
 def test_modify_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="group_name1", header="group_head1", footer="group_foot1"))
-    app.group.modify_first_group(Group(name="group_name2", header="group_head2", footer="group_foot2"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="group_name2", header="group_head2", footer="group_foot2")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_modify_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="group_name1", header="group_head1", footer="group_foot1"))
-    app.group.modify_first_group(Group(name="New group"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="New header")
+    group.id = old_groups[0].id
+    app.group.modify_first_group(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
-
-def test_modify_header(app):
-    if app.group.count() == 0:
-        app.group.create(Group(name="group_name1", header="group_head1", footer="group_foot1"))
-    app.group.modify_first_group(Group(header="New header"))
 
