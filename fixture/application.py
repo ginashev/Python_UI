ad28a075c073391854cf23386832880e93993125
GinaShev <ginashev@gmail.com>
2021-09-09 22:52:39 +0200

    small fixes

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,5 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-from fixture.sessionHelper import SessionHelper
+from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
